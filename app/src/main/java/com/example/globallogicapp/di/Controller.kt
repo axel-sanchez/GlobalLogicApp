298b50a6b97020de04211504e841dbb0fe6315c8
Axel <axelsan97@hotmail.com>
2021-05-26 01:00:59 -0300

    patron de diseño builder

--- app/src/main/java/com/example/globallogicapp/di/Controller.kt
+++ app/src/main/java/com/example/globallogicapp/di/Controller.kt
@@ -3,14 +3,13 @@ package com.example.globallogicapp.di
 import androidx.room.Room
 import com.example.globallogicapp.data.repository.ProductRepositoryImpl
 import com.example.globallogicapp.data.room.Database
-import com.example.globallogicapp.data.room.ProductDao
 import com.example.globallogicapp.data.service.ApiServiceProduct
 import com.example.globallogicapp.data.source.ProductRemoteSource
 import com.example.globallogicapp.data.source.ProductRemoteSourceImpl
 import com.example.globallogicapp.domain.repository.ProductRepository
 import com.example.globallogicapp.domain.usecase.GetAllProductsUseCase
 import com.example.globallogicapp.domain.usecase.GetAllProductsUseCaseImpl
-import com.example.globallogicapp.data.service.ApiFactory
+import com.example.globallogicapp.data.service.ApiClient
 import com.example.globallogicapp.data.source.ProductLocalSource
 import com.example.globallogicapp.data.source.ProductLocalSourceImpl
 import com.example.globallogicapp.domain.usecase.GetProductUseCase
@@ -23,7 +22,10 @@ import org.koin.dsl.module
  * @author Axel Sanchez
  */
 val moduleApp = module {
-    factory<ApiServiceProduct> { ApiFactory.getApiClient(BASE_URL).create(ApiServiceProduct::class.java) }
+    single { ApiClient.Builder<ApiServiceProduct>()
+        .setBaseUrl(BASE_URL)
+        .setApiService(ApiServiceProduct::class.java)
+        .build()}
     single<ProductRepository> { ProductRepositoryImpl(get() as ProductRemoteSource, get() as ProductLocalSource) }
     single<ProductRemoteSource> { ProductRemoteSourceImpl(get() as ApiServiceProduct) }
     single<ProductLocalSource> { ProductLocalSourceImpl((get() as Database).productDao()) }
